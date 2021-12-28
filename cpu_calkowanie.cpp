8ccd448bf33b34e0e8ea29a3162e402524085a03
Dianamoon18 <72127610+Diamoon18@users.noreply.github.com>
2021-12-28 17:40:28 +0100

    Create cpu_calkowanie.cpp

---
+++ cpu_calkowanie.cpp
@@ -0,0 +1,88 @@
+%%writefile cpu_calkowanie.cpp
+#include <iostream>
+#include <cstdlib>
+#include<stdio.h>
+
+/*
+%%shell
+c++ cpu_calkowanie.cpp -o cpu
+./cpu
+*/
+
+using namespace std;
+
+float function1(float x) { return x*x; }
+
+float cpu_prostokaty(float xp, float xk, int n){
+    float h, calka = 0;
+    h = (xk - xp) / (float)n;
+    for (int i = 1; i <= n; i++)
+    {
+        calka += function1(xp + i*h)*h;
+    }
+    return calka;
+}
+
+float cpu_trapezy(float xp, float xk, int n){
+    float h, calka = 0;
+    h = (xk - xp) / (float)n;
+    for (int i = 1; i < n; i++)
+    {
+        calka += function1(xp + i * h);
+    }
+    calka += function1(xp) / 2;
+    calka += function1(xk) / 2;
+    calka *= h;
+    return calka;
+}
+
+float cpu_Simpson(float xp, float xk, int n){
+    float h, x, calka = 0;
+    float st = 0;
+    h = (xk - xp) / (float)n;
+    for(int i = 1; i <= n; i++)
+    {
+      x = xp + i * h;
+      st += function1(x - h/2);
+      if(i < n) calka += function1(x);
+    }
+    calka = h / 6*(function1(xp) + function1(xk) + 2*calka + 4*st);
+    return calka;
+}
+
+int main()
+{
+    clock_t start, end;
+    float xp, xk;
+ 
+    xp = -4;
+    xk = 2;
+ 
+    for(int n = 10; n <= 100000; n*=10){
+        /*
+        start = clock(); 
+        cout << "Wynik: " <<  cpu_prostokaty(xp, xk, n) << endl;
+        end = clock();
+        double cpu_prostok = ((double)(end-start))/CLOCKS_PER_SEC; 
+        printf("CPU prostokaty n=%d: %lf sekund\n", n, cpu_prostok);
+        cout << "\n"<< endl;
+        
+
+        start = clock(); 
+        cout << "Wynik: " <<  cpu_trapezy(xp, xk, n) << endl;
+        end = clock();
+        double cpu_trapezy = ((double)(end-start))/CLOCKS_PER_SEC; 
+        printf("CPU trapezy n=%d: %lf sekund\n", n, cpu_trapezy);
+        cout << "\n"<< endl;
+        */
+        
+        start = clock(); 
+        cout << "Wynik: " <<  cpu_Simpson(xp, xk, n) << endl;
+        end = clock();
+        double cpu_simp = ((double)(end-start))/CLOCKS_PER_SEC; 
+        printf("CPU simpson n=%d: %lf sekund\n", n, cpu_simp);
+        cout << "\n"<< endl;
+        
+    }
+    return 0;
+}
